486588ed7ef48132b6c7dc43a38802599b956306
Damien <damien@dam.io>
2020-07-24 23:03:11 +0200

    add more attributes tests

--- tests.js
+++ tests.js
@@ -1,13 +1,32 @@
 
 it('shoudl render hello world', () => {
-  chai.assert.equal(L.render(L.h1("hello world")), "<h1>hello world</h1>");
-});
+  chai.assert.equal(L.render(L.h1("hello world")), "<h1>hello world</h1>")
+})
 
 it('should render attributes', () => {
-  chai.assert.equal(L.render(L.h1('world', {class:'hello'})),
-    '<h1 class="hello">world</h1>')
-});
-it('should support reverse attributes calling', () => {
+  chai.assert.equal(L.render(
+  	L.h1('world', {class:'hello'})),
+    '<h1 class="hello">world</h1>'
+  )
+  chai.assert.equal(L.render(
+  	L.input('hello', {id: 'hello', value:'world'})),
+    '<input id="hello" value="world">'
+  )
+
+  /* not supported yet
+  chai.assert.equal(L.render(
+  	L.input('hello', {what: null})),
+    '<input what>'
+  )
+  */
+
+  chai.assert.equal(L.render(
+  	L.input({"data-trigger": 666})),
+    '<input data-trigger="666">'
+  )
+})
+
+it('should support attributes first', () => {
   chai.assert.equal(L.render(L.h1({class:'hello'}, 'world')),
     '<h1 class="hello">world</h1>')
 })
